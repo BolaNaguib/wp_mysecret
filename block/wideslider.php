81afb5e040ab35c56a6152fa34e1f7b392f8bf04
Bola <bolanaguib@gmail.com>
2020-06-11 04:09:28 +0200

    [ Cleaned everything]

--- block/wideslider.php
+++
@@ -1,6 +0,0 @@
-<!-- WIDESLIDER --><!-- START section_type-slider --><div class="section_type-wideslider"><!-- START uk-container --><div class="uk-container"><div uk-slideshow="animation: fade"><div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1"> <?php if (have_rows('slider')) : ?> <ul class="uk-slideshow-items"> <?php while (have_rows('slider')) : the_row();
-                            //ACF Fields
-                            $image = get_sub_field('image');
-                            $page = get_sub_field('page');
-                        ?> <li><img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>" uk-cover></li> <?php endwhile; ?> </ul> <?php endif; ?> <!-- <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-                <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a> --><!-- START slidenavContainer --><div class="slidenavContainer"><ul class="uk-slideshow-nav uk-dotnav uk-flex-center uk-margin"></ul></div><!-- END slidenavContainer --></div></div></div><!-- END uk-container --></div><!-- END section_type-slider -->
