a2094493287b4f9efd45d30f4a8eb17d6fb03c51
Bola <bolanaguib@gmail.com>
2020-06-13 23:21:29 +0200

    [Added] must Have products Block

--- src/template/block/musthave.php
+++ src/template/block/musthave.php
@@ -1,9 +1,20 @@
- <!-- START section-musthave -->
- <section class='section-musthave'>
-     <!-- START uk-container -->
-     <div class='uk-container'>
-         hello
-     </div>
-     <!-- END uk-container -->
- </section>
- <!-- END section-musthave -->
+<!-- START section-musthave -->
+<section class='section-musthave'>
+    <!-- START uk-container -->
+    <div class='uk-container'>
+        <!-- START uk-grid -->
+        <div class='uk-grid uk-grid-match'>
+            <?php
+            if ($query->have_posts()) :
+                while ($query->have_posts()) : $query->the_post();
+                    global $product;
+            ?>
+                    sssss
+            <?php endwhile;
+            endif; ?>
+        </div>
+        <!-- END uk-grid -->
+    </div>
+    <!-- END uk-container -->
+</section>
+<!-- END section-musthave -->
