35e77cf7c4ca42309be32aea5889859ebf6bff39
Bola <bolanaguib@gmail.com>
2020-06-14 08:00:08 +0200

    [ Modified ] Content-product.php

--- src/template/block/musthave.php
+++ src/template/block/musthave.php
@@ -1,3 +1,15 @@
+<?php $term = get_field('productcategory');
+if ($term) :
+    $query = new WP_Query(array(
+        'post_type' => 'product',
+        'product_cat' => $term->slug,
+    ));
+else :
+    $query = new WP_Query(array(
+        'post_type' => 'product',
+    ));
+endif;
+?>
 <!-- START section-musthave -->
 <section class='section-musthave'>
     <!-- START uk-container -->
@@ -9,9 +21,89 @@
                 while ($query->have_posts()) : $query->the_post();
                     global $product;
             ?>
-                    sssss
+
+                    <div <?php wc_product_class('uk-width-1-5@m uk-margin-bottom uk-text-center', $product); ?>>
+                        <?php
+                        /**
+                         * Hook: woocommerce_before_shop_loop_item.
+                         *
+                         * @hooked woocommerce_template_loop_product_link_open - 10
+                         */
+                        // do_action('woocommerce_before_shop_loop_item');
+
+                        ?>
+                        <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($loop->post->ID), 'single-post-thumbnail'); ?>
+                        <!-- START card_type-product -->
+                        <div class='card_type-product uk-text-center'>
+
+
+                            <!-- START uk-position-relative -->
+                            <div class='uk-position-relative'>
+                                <!-- START imageContainer -->
+                                <div class='imageContainer'>
+                                    <img src="<?php echo $image[0]; ?>" alt="">
+                                    <!-- START imageOverlay -->
+                                    <div class='imageOverlay'>
+                                        <!-- START uk-grid uk-grid-match uk-grid-collapse -->
+                                        <div class='uk-grid uk-grid-match uk-grid-collapse'>
+                                            <!-- START uk-width-1-2 -->
+                                            <div class='uk-width-1-2'>
+                                                <!-- START Wishlist -->
+                                                <div class='Wishlist'>
+                                                    <?php echo do_shortcode('[ti_wishlists_addtowishlist]') ?>
+                                                </div>
+                                                <!-- END Wishlist -->
+                                            </div>
+                                            <!-- END uk-width-1-2 -->
+                                            <!-- START uk-width-1-2 -->
+                                            <div class='uk-width-1-2'>
+                                                <!-- START detailes -->
+                                                <div class='detailes'>
+                                                    <a href="<?php echo get_permalink($loop[0]->post->ID) ?>"> detailes </a>
+
+                                                </div>
+                                                <!-- END Wishlist -->
+                                            </div>
+                                            <!-- END uk-width-1-2 -->
+                                        </div>
+                                        <!-- END uk-grid uk-grid-match uk-grid-collapse -->
+                                    </div>
+                                    <!-- END imageOverlay -->
+                                </div>
+                                <!-- END imageContainer -->
+                                <?php if ($product->stock_status != "instock") : ?>
+                                    <span class="outofstock uk-flex uk-flex-middle uk-flex-center">Out of Stock</span>
+                                <?php endif; ?>
+                                <?php if ($product->sale_price != "") : ?>
+                                    <span class="sale">Sale</span>
+
+                                <?php endif; ?>
+
+                            </div>
+                            <!-- END uk-position-relative -->
+
+
+                            <!-- START uk-text-center -->
+                            <div class='uk-text-center uk-margin-top'>
+                                <span class="title"><?php echo the_title(); ?></span>
+                                <?php if ($price_html = $product->get_price_html()) : ?>
+                                    <span class="price"><?php echo $price_html; ?></span>
+                                <?php endif; ?>
+                            </div>
+                            <!-- END uk-text-center -->
+
+
+                        </div>
+                        <!-- END card_type-product -->
+
+                        <?php
+                        // do_action('woocommerce_after_shop_loop_item');
+                        ?>
+                    </div>
             <?php endwhile;
-            endif; ?>
+            else : echo wpautop('Sorry, no posts were found');
+            endif;
+            wp_reset_postdata(); ?>
         </div>
         <!-- END uk-grid -->
     </div>
--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -37,6 +37,7 @@ if (post_password_required()) {
 	<!-- START uk-container -->
 	<div class='uk-container'>
 
+		<hr>
 		<div id="product-<?php the_ID(); ?>" <?php wc_product_class('', $product); ?>>
 
 			<?php
