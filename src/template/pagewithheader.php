152302e25176cb4d4a9b4cfbe29145907d302f6a
Bola <bolanaguib@gmail.com>
2020-07-15 22:14:32 +0200

    [ modified ] page with header

--- src/template/pagewithheader.php
+++ src/template/pagewithheader.php
@@ -21,12 +21,17 @@ $hero_caption = get_field('hero_caption');
     <!-- END uk-container -->
 
 </section>
-<?php if (have_posts()) : ?>
-    <?php /* Start the Loop */ ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <?php the_content(); ?>
-    <?php endwhile; ?>
-<?php else : ?>
-<?php endif; ?>
+<!-- START uk-container -->
+<div class='uk-container'>
+    <?php if (have_posts()) : ?>
+        <?php /* Start the Loop */ ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+    <?php else : ?>
+    <?php endif; ?>
+</div>
+<!-- END uk-container -->
+
 
 <?php get_footer(); ?>
