49fd75152ce6cb0cda3222a03118e1c340329ce5
Bola <bolanaguib@gmail.com>
2020-07-15 22:26:58 +0200

    [ added ] default image if no pic

--- src/template/pagewithheader.php
+++ src/template/pagewithheader.php
@@ -2,6 +2,7 @@
 
 <?php get_header(); ?>
 <?php $hero_wallpaper = get_field('hero_wallpaper');
+$hero_bg = $hero_wallpaper ? $hero_wallpaper : 'https://gl-images.condecdn.net/image/o7eLjdokLWq/crop/2040/0.46875/f/gettyimages-471453081_sf.jpg'
 $hero_title = get_field('hero_title');
 $hero_caption = get_field('hero_caption');
 ?>
