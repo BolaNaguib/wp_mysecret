98df7bee6026aa840477facda0dee20b2c780c67
Bola <bolanaguib@gmail.com>
2020-07-15 22:27:50 +0200

    [ fixed ] typo

--- src/template/pagewithheader.php
+++ src/template/pagewithheader.php
@@ -2,12 +2,12 @@
 
 <?php get_header(); ?>
 <?php $hero_wallpaper = get_field('hero_wallpaper');
-$hero_bg = $hero_wallpaper ? $hero_wallpaper : 'https://gl-images.condecdn.net/image/o7eLjdokLWq/crop/2040/0.46875/f/gettyimages-471453081_sf.jpg'
+$hero_bg = $hero_wallpaper ? $hero_wallpaper : 'https://gl-images.condecdn.net/image/o7eLjdokLWq/crop/2040/0.46875/f/gettyimages-471453081_sf.jpg';
 $hero_title = get_field('hero_title');
 $hero_caption = get_field('hero_caption');
 ?>
 
-<section class="section section_type-normal" style="background-image: url('<?php echo $hero_wallpaper; ?>');">
+<section class="section section_type-normal" style="background-image: url('<?php echo $hero_bg; ?>');">
     <!-- START uk-container -->
     <div class='uk-container uk-position-relative uk-text-center'>
         <?php if ($hero_title) : ?>
