8020dba1338cc7d910ed44d7ba32ef916e51a570
Bola <bolanaguib@gmail.com>
2020-07-26 18:51:08 +0200

    [ fixed ] loading icon

--- src/template/header.php
+++ src/template/header.php
@@ -98,7 +98,7 @@
     <div class='loadercontainerx'>
         <!-- START loader -->
         <div class=''>
-            <img style="width:100%; height:100%" src="<?php echo get_template_directory_uri() ?>/images/loader.gif" alt="">
+            <img style="width:100%; height:100vh" src="<?php echo get_template_directory_uri() ?>/images/loader.gif" alt="">
         </div>
 
         <!-- END loader -->
