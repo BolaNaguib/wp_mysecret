0d11232840f7a8efde8e59ac2343009a93cb63d8
Bola <bolanaguib@gmail.com>
2020-09-20 02:54:16 +0200

    [ Added ] Coming Soon

--- comingSoon.php
+++ comingSoon.php
@@ -1,1 +1,1 @@
-<?php /* Template Name: coming Soon Template */ ?> We Are Working on it Don't Worry :)
+<?php /* Template Name: coming Soon Template */ ?> <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>MySecret - Coming Soon</title> <?php wp_head(); ?> </head><body><img style="width:100%; height:100vh" src="<?php echo get_template_directory_uri() ?>/images/comingsoon.jpg" alt=""> <?php wp_footer(); ?> </body></html>
--- src/template/comingSoon.php
+++ src/template/comingSoon.php
@@ -1,4 +1,19 @@
 <?php /* Template Name: coming Soon Template */ ?>
 
 
-We Are Working on it Don't Worry :)
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MySecret - Coming Soon</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <img style="width:100%; height:100vh" src="<?php echo get_template_directory_uri() ?>/images/comingsoon.jpg" alt="">
+    <?php wp_footer(); ?>
+</body>
+
+</html>
