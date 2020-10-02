aa08c79fe3db438245753a3793d9025549b923bc
Bola <bolanaguib@gmail.com>
2020-10-02 21:56:21 +0200

    [ Added ] big loader

--- header.php
+++ header.php
@@ -1,133 +1,188 @@
 <!DOCTYPE html><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><html><head> <?php
-    // ACF FIELDS 
-    $fb = get_field('facebook', 'options');
+  // ACF FIELDS 
+  $fb = get_field('facebook', 'options');
 
-    $ig = get_field('instagram', 'options');
-    $address = get_field('address', 'options');
-    $phone = get_field('phone', 'options');
-    $phone_display = get_field('phone_display', 'options');
-    $logo = get_field('logo', 'option');
-    ?> <meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title> <?php the_field('meta_title'); ?> </title><meta name="title" content=" <?php the_field('meta_title'); ?> "><meta name="keywords" content=" <?php the_field('meta_keywords'); ?> "><meta name="description" content=" <?php the_field('meta_description'); ?> "><link rel="shortcut icon" type="image/x-icon" href="<?php the_field('favicon', 'option') ?>"> <?php the_field('header_js_general', 'option');
-    the_field('header_js'); ?> <?php wp_head(); ?> <script>function PbStyleQuantite(a) {
-            var b, c = !1;
-            a || (a = ".qty"),
-                b = jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").find(a),
-                b.length && (jQuery.each(b, function(a, b) {
-                        "date" !== jQuery(b).prop("type") && "hidden" !== jQuery(b).prop("type") && (jQuery(b).parent().hasClass("buttons_added") || (jQuery(b).parent().addClass("buttons_added").prepend('<input type="button" value="-" class="minus" />'),
-                            jQuery(b).addClass("input-text").after('<input type="button" value="+" class="plus" />'),
-                            c = !0))
-                    }),
-                    c && (jQuery("input" + a + ":not(.product-quantity input" + a + ")").each(function() {
-                            var a = parseFloat(jQuery(this).attr("min"));
-                            a && a > 0 && parseFloat(jQuery(this).val()) < a && jQuery(this).val(a)
-                        }),
-                        jQuery(".plus, .minus").unbind("click"),
-                        jQuery(".plus, .minus").on("click", function() {
-                            var b = jQuery(this).parent().find(a),
-                                c = parseFloat(b.val()),
-                                d = parseFloat(b.attr("max")),
-                                e = parseFloat(b.attr("min")),
-                                f = b.attr("step");
-                            c && "" !== c && "NaN" !== c || (c = 0),
-                                "" !== d && "NaN" !== d || (d = ""),
-                                "" !== e && "NaN" !== e || (e = 0),
-                                "any" !== f && "" !== f && void 0 !== f && "NaN" !== parseFloat(f) || (f = 1),
-                                jQuery(this).is(".plus") ? d && (d == c || c > d) ? b.val(d) : b.val(c + parseFloat(f)) : e && (e == c || c < e) ? b.val(e) : c > 0 && b.val(c - parseFloat(f)),
-                                b.trigger("change")
-                        })))
-        }
-        jQuery(window).on("load", function() {
-                PbStyleQuantite()
+  $ig = get_field('instagram', 'options');
+  $address = get_field('address', 'options');
+  $phone = get_field('phone', 'options');
+  $phone_display = get_field('phone_display', 'options');
+  $logo = get_field('logo', 'option');
+  ?> <meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title> <?php the_field('meta_title'); ?> </title><meta name="title" content=" <?php the_field('meta_title'); ?> "><meta name="keywords" content=" <?php the_field('meta_keywords'); ?> "><meta name="description" content=" <?php the_field('meta_description'); ?> "><link rel="shortcut icon" type="image/x-icon" href="<?php the_field('favicon', 'option') ?>"> <?php the_field('header_js_general', 'option');
+  the_field('header_js'); ?> <?php wp_head(); ?> <script>function PbStyleQuantite(a) {
+      var b, c = !1;
+      a || (a = ".qty"),
+        b = jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").find(a),
+        b.length && (jQuery.each(b, function(a, b) {
+            "date" !== jQuery(b).prop("type") && "hidden" !== jQuery(b).prop("type") && (jQuery(b).parent().hasClass("buttons_added") || (jQuery(b).parent().addClass("buttons_added").prepend('<input type="button" value="-" class="minus" />'),
+              jQuery(b).addClass("input-text").after('<input type="button" value="+" class="plus" />'),
+              c = !0))
+          }),
+          c && (jQuery("input" + a + ":not(.product-quantity input" + a + ")").each(function() {
+              var a = parseFloat(jQuery(this).attr("min"));
+              a && a > 0 && parseFloat(jQuery(this).val()) < a && jQuery(this).val(a)
             }),
-            jQuery(document).ajaxComplete(function() {
-                PbStyleQuantite()
-            });</script></head><body <?php body_class(); ?>><!-- Load Facebook SDK for JavaScript --><div id="fb-root"></div><script>window.fbAsyncInit = function() {
-  FB.init({
-    xfbml            : true,
-    version          : 'v7.0'
-  });
-};
+            jQuery(".plus, .minus").unbind("click"),
+            jQuery(".plus, .minus").on("click", function() {
+              var b = jQuery(this).parent().find(a),
+                c = parseFloat(b.val()),
+                d = parseFloat(b.attr("max")),
+                e = parseFloat(b.attr("min")),
+                f = b.attr("step");
+              c && "" !== c && "NaN" !== c || (c = 0),
+                "" !== d && "NaN" !== d || (d = ""),
+                "" !== e && "NaN" !== e || (e = 0),
+                "any" !== f && "" !== f && void 0 !== f && "NaN" !== parseFloat(f) || (f = 1),
+                jQuery(this).is(".plus") ? d && (d == c || c > d) ? b.val(d) : b.val(c + parseFloat(f)) : e && (e == c || c < e) ? b.val(e) : c > 0 && b.val(c - parseFloat(f)),
+                b.trigger("change")
+            })))
+    }
+    jQuery(window).on("load", function() {
+        PbStyleQuantite()
+      }),
+      jQuery(document).ajaxComplete(function() {
+        PbStyleQuantite()
+      });</script></head><body <?php body_class(); ?>><!-- Load Facebook SDK for JavaScript --><div id="fb-root"></div><script>window.fbAsyncInit = function() {
+      FB.init({
+        xfbml: true,
+        version: 'v7.0'
+      });
+    };
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script><!-- Your Chat Plugin code --><div class="fb-customerchat" attribution="install_email" page_id="987161051491898"></div><style>.loadercontainerx {
-            width: 100%;
-            height: 100%;
-            position: absolute;
-            top: 0px;
-            background-color: #000;
-            color: #fff;
-            z-index: 9999999;
-            display: block;
-            transition: 300ms;
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script><!-- Your Chat Plugin code --><div class="fb-customerchat" attribution="install_email" page_id="987161051491898"></div><style>.loadercontainerx {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0px;
+      background-color: #000;
+      color: #fff;
+      z-index: 9999999;
+      display: block;
+      transition: 300ms;
 
-opacity:1;
-        }
+      opacity: 1;
+    }
 
-        .loader {
-            width: 100%;
-            height: 100%;
-            background-color: #ffc6df;
 
-        }
 
-        .loader img {
-            border: 2px solid #fff;
-        }
+    .loaded {
+      /* display: none; */
+      opacity: 0;
+      transition: 300ms;
+      z-index: -1;
+    }
 
-        .loaded {
-            /* display: none; */
-            opacity: 0;
-            transition: 300ms;
-            z-index: -1;
-        }
-        .spinner {
-  
-  text-align: center;
-  display: inline-block;
-}
+    .loading {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: #000;
+      z-index: 9999;
+    }
 
-.spinner > div {
-  width: 5px;
-  height: 5px;
-  background-color: #fff;
+    .loading-text {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      margin: auto;
+      text-align: center;
+      width: 100%;
+      height: 100px;
+      line-height: 100px;
+    }
 
-  border-radius: 100%;
-  display: inline-block;
-  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
-  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
-}
+    .loading-text span {
+      width: 10%;
+      display: inline-block;
+      margin: 0 5px;
+      color: #fff;
+      font-size: 80px;
+    }
 
-.spinner .bounce1 {
-  -webkit-animation-delay: -0.32s;
-  animation-delay: -0.32s;
-}
+    .loading-text span:nth-child(1) {
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      -webkit-animation: blur-text 1.5s 0s infinite linear alternate;
+      animation: blur-text 1.5s 0s infinite linear alternate;
+    }
 
-.spinner .bounce2 {
-  -webkit-animation-delay: -0.16s;
-  animation-delay: -0.16s;
-}
+    .loading-text span:nth-child(2) {
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      -webkit-animation: blur-text 1.5s 0.2s infinite linear alternate;
+      animation: blur-text 1.5s 0.2s infinite linear alternate;
+    }
 
-@-webkit-keyframes sk-bouncedelay {
-  0%, 80%, 100% { -webkit-transform: scale(0) }
-  40% { -webkit-transform: scale(1.0) }
-}
+    .loading-text span:nth-child(3) {
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      -webkit-animation: blur-text 1.5s 0.4s infinite linear alternate;
+      animation: blur-text 1.5s 0.4s infinite linear alternate;
+    }
 
-@keyframes sk-bouncedelay {
-  0%, 80%, 100% { 
-    -webkit-transform: scale(0);
-    transform: scale(0);
-  } 40% { 
-    -webkit-transform: scale(1.0);
-    transform: scale(1.0);
-  }
-}</style><!-- START loadercontainerx --><div class="loadercontainerx uk-text-center"><!-- START loader --><div class="" style="height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;"><span style="margin-right:4px;">My Secret</span><div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div><!-- END loader --></div><!-- END loadercontainerx --> <?php include get_template_directory() . '/common/topmenu.php'; ?> <?php include get_template_directory() . '/common/mainmenudesktop.php'; ?> <?php include get_template_directory() . '/common/mobilemenu.php'; ?> <?php include get_template_directory() . '/common/loginmodal.php'; ?> <?php global $wpdb;
+    .loading-text span:nth-child(4) {
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      -webkit-animation: blur-text 1.5s 0.6s infinite linear alternate;
+      animation: blur-text 1.5s 0.6s infinite linear alternate;
+    }
 
-    ?> <!-- </div> --></body></html>
+    .loading-text span:nth-child(5) {
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      -webkit-animation: blur-text 1.5s 0.8s infinite linear alternate;
+      animation: blur-text 1.5s 0.8s infinite linear alternate;
+    }
+
+    .loading-text span:nth-child(6) {
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      -webkit-animation: blur-text 1.5s 1s infinite linear alternate;
+      animation: blur-text 1.5s 1s infinite linear alternate;
+    }
+
+    .loading-text span:nth-child(7) {
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      -webkit-animation: blur-text 1.5s 1.2s infinite linear alternate;
+      animation: blur-text 1.5s 1.2s infinite linear alternate;
+    }
+
+    .loading-text span:nth-child(8) {
+      -webkit-filter: blur(0px);
+      filter: blur(0px);
+      -webkit-animation: blur-text 1.5s 1.4s infinite linear alternate;
+      animation: blur-text 1.5s 1.4s infinite linear alternate;
+    }
+
+    @-webkit-keyframes blur-text {
+      0% {
+        opacity: 0;
+      }
+
+      100% {
+        opacity: 1;
+      }
+    }
+
+    @keyframes blur-text {
+      0% {
+        opacity: 0;
+      }
+
+      100% {
+        opacity: 1;
+      }
+    }</style><!-- START loadercontainerx --><div class="loadercontainerx uk-text-center"><div class="loading"><div class="loading-text"><span class="loading-text-words">M</span> <span class="loading-text-words">Y</span> <span class="loading-text-words">S</span> <span class="loading-text-words">E</span> <span class="loading-text-words">C</span> <span class="loading-text-words">R</span> <span class="loading-text-words">E</span> <span class="loading-text-words">T</span></div></div><!-- END loader --></div><!-- END loadercontainerx --> <?php include get_template_directory() . '/common/topmenu.php'; ?> <?php include get_template_directory() . '/common/mainmenudesktop.php'; ?> <?php include get_template_directory() . '/common/mobilemenu.php'; ?> <?php include get_template_directory() . '/common/loginmodal.php'; ?> <?php global $wpdb;
+
+  ?> <!-- </div> --></body></html>
